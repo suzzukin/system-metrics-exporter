384bbda90f65c66360b944b92512461b95382cd7
Artem Sharifullin <a.sharifullin@it-bastion.com>
2025-06-15 17:57:54 +0200

    fix update script

--- update.sh
+++ update.sh
@@ -107,7 +107,10 @@ update_script() {
     print_message "Updating script..." "$YELLOW"
 
     # Install new version
-    mv "$TEMP_DIR/node-metrics-exporter.new" /usr/local/bin/node-metrics-exporter
+    mv "$TEMP_DIR/node-metrics-exporter.new" /usr/local/bin/node-metrics-exporter || {
+        print_message "Failed to move new version to /usr/local/bin" "$RED"
+        exit 1
+    }
 
     # Cleanup
     cd - > /dev/null
